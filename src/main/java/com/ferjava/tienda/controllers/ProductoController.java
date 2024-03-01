0db2880256114db4db2c6c54068410ffd3cb685e
Fernando Perez <fernando_perez01@yahoo.es>
2024-03-01 02:34:18 -0500

    con reccione en metodo request

--- src/main/java/com/ferjava/tienda/controllers/ProductoController.java
+++ src/main/java/com/ferjava/tienda/controllers/ProductoController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin("*")
 public class ProductoController {
 
     @Autowired
@@ -62,7 +63,7 @@ public class ProductoController {
         return ResponseEntity.ok(this.productoRepository.findAll());
     }
 
-    @PostMapping("/actualizar")
+    @PutMapping("/actualizar")
     public ResponseEntity<?> actualizarProducto(@RequestBody CreateProductoDTO createProductoDTO){
         ProductoEntity productoEntity = new ProductoEntity();
                         productoEntity.setId(createProductoDTO.getId());
